079adb8b1efbf2f24c9e132d24d62f8dbb09d63c
Simon Chiang <simon.a.chiang@gmail.com>
2012-07-13 20:20:36 -0600

    Add help and instructions to util scripts

--- util/archive.sh
+++ util/archive.sh
@@ -1,18 +1,44 @@
-#!/bin/sh
-# Download a snapshot of the Git mailing list archive from [gmane](http://gmane.org/).
-# From project root:
-#
-#   sh ./util/archive.sh
-#
+#!/bin/bash
+############################################################################
+progname="${0##*/}"
+author="Simon Chiang"
+usage="usage: %s [-h] GROUP [PAGE...]\n"
+opt="       %s   %s\n"
 
-start=1849
-stop=1856
-page=$start
-archive_dir="archive"
-
-mkdir -p "${archive_dir}"
-while [ ${page} -lt $((stop + 1)) ]
+while getopts "h" option
 do
-	curl "http://news.gmane.org/group/gmane.comp.version-control.git/last=0/force_load=very/?page=${page}&action=--Action--" > ${archive_dir}/${page}.html
-	page=$(( page + 1 ))
+  case $option in
+    h  )  printf "$usage" "$progname"
+          printf '
+  Download pages from gmane.  Combine with parse to view pages as text.
+
+    ./archive.sh gmane.comp.version-control.git 0 1 2 | ./parse_archive
+
+  Produces output like:
+
+    1205 -- Colorized git log
+    `- 1208 -- Colorized git log
+     `- 1224 -- [PATCH] Colorized git log
+      `- 1225 -- [PATCH] Colorized git log
+       |- 1227 -- [PATCH] Colorized git log
+       `- 1226 -- [PATCH] Colorized git log
+
+'
+          printf "options:\n\n"
+          printf "$opt" "-h" "prints this help"
+          printf "\n"
+          exit 0 ;;
+    \? )  printf "$usage" "$progname"
+          exit 2 ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+group="${1}"
+shift 1
+
+############################################################################
+
+for page
+do curl -s "http://news.gmane.org/group/$group?page=${page}"
 done
